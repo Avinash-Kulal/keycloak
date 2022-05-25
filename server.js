44904c003d175cc4344ee90af50fca4592e448d5
Avinash Kulal <avinash.kulal@juegostudioz.com>
2022-05-25 18:05:13 +0530

    redirect to keycloack for login and register

--- server.js
+++ server.js
@@ -2,15 +2,30 @@ const app = require("express")();
 const session = require("express-session");
 require("dotenv-defaults").config();
 
+
+app.get('/',(req,res)=>{
+	res.send('Home page')
+})
+app.get('/login',(req,res)=>{
+ return res.redirect(process.env.SERVER_LOGIN_URL)
+})
+
+app.get('/logout',(req,res)=>{
+	return res.redirect(process.env.SERVER_LOGOUT_URL)
+})
+
+app.get('/register',(req,res)=>{
+	return res.redirect(process.env.SERVER_REGISTER_URL)
+})
+app.get('/protected',(req,res)=>{
+ res.send('<h1>Welcome You are logged in</h1> | <a href="/logout">Logout</a>');
+})
+
 const keycloak = require('./config.keycloak.js').initializeKeycloak();
 app.use(keycloak.middleware());
 
+
 const authRouter = require('./router');
-
-
-app.get('/',(req,res)=>{
-	res.send('Welcome');
-});
 
 
 app.use('/auth',authRouter);
