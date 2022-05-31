00926e7a7c52f1ec3226dd13d434de7ee13b0ad2
Avinash Kulal <avinash.kulal@juegostudioz.com>
2022-05-31 14:09:18 +0530

    get user name from id_token

--- router.js
+++ router.js
@@ -7,7 +7,6 @@ router.get('/anonymous',function(req,res){
 })
 
 router.get('/user',keycloak.protect('user'),function(req,res){
- console.log(req.kauth)
  res.send('<h3>User permission | <a href="/logout">logout</a></h3>')
 })
 
@@ -19,8 +18,9 @@ router.get('/all-user',keycloak.protect(['user','admin']),function(req,res){
  res.send('<h3>All user permission | <a href="/logout">logout</a></h3>')
 })
 router.get('/protected',keycloak.protect(),(req,res)=>{
- 
- res.send('<center><h1>Welcome You are logged in</h1> <h3>  <br><br> <a href="/auth/user">User</a> <br><br> <a href="/auth/admin">Admin</a> <br><br><a href="/logout">Logout</a></h3></center> ');
+ var content = req.kauth.grant.id_token.content;
+ var name = content.name;
+ res.send(`<center><h1>Welcome : <span style="color:red;text-decoration:underline">${name}</span> <br>You are logged in</h1> <h3>  <br><br> <a href="/auth/user">User</a> <br><br> <a href="/auth/admin">Admin</a> <br><br><a href="/logout">Logout</a></h3></center> `);
 })
 
 module.exports = router;
